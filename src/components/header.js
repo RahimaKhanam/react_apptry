1aa0ee2aed911183d727e4fb434246eb10d680d8
RahimaKhanam <rahima.khanam45@gmail.com>
2021-05-25 03:01:02 +0530

    Styles method #1

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 
+let styles = {
+    header: {
+        background: '#03a9f4'
+    },
+    logo: {
+        color: '#fff',
+        fontFamily: 'Anton',
+        textAlign: 'center'
+    }
+}
 
 class Header extends Component {
     render (){
         return (
-            <header>
-                <div>Logo</div>
+            <header style={styles.header}>
+                <div style={styles.logo}>Logo</div>
                 <input/>
             </header>
         )
