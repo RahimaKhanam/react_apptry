a91591f4b1337ef08afc2e0df10b3ca39833f71d
RahimaKhanam <rahima.khanam45@gmail.com>
2021-05-25 03:21:42 +0530

    #2 Using styles.css inside public direc

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,11 @@
 import React, { Component } from 'react';
 
-let styles = {
-    header: {
-        background: '#03a9f4'
-    },
-    logo: {
-        color: '#fff',
-        fontFamily: 'Anton',
-        textAlign: 'center'
-    }
-}
 
 class Header extends Component {
     render (){
         return (
-            <header style={styles.header}>
-                <div style={styles.logo}>Logo</div>
+            <header>
+                <div className='logo'>Logo</div>
                 <input/>
             </header>
         )
